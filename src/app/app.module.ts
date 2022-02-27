cd5f66f287c2da88b8e860a9f9b83c92291a1150
zhiquan114514 <zhiquan@email.sc.edu>
2022-02-27 02:59:53 -0500

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { environment } from "../environments/environment";
+import { initializeApp } from "firebase/app";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+initializeApp(environment.firebase);
 @NgModule({
   declarations: [
     AppComponent
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,20 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+
+    apiKey: "AIzaSyBIKj9T8JeG5_gCO1kuBCwpbT2tTIo1I6c",
+    authDomain: "bitsquad-5f2bf.firebaseapp.com",
+    databaseURL: "https://bitsquad-5f2bf-default-rtdb.firebaseio.com",
+    projectId: "bitsquad-5f2bf",
+    storageBucket: "bitsquad-5f2bf.appspot.com",
+    messagingSenderId: "153813992768",
+    appId: "1:153813992768:web:390c61924ad7480e9466a5",
+    measurementId: "G-J0W9J7H77Y",
+    vapidKey:"BJpYp3x8HA8AWMpst_Qz6rAy2i8cF55wlEV7mR5vhT3QAj55ZE2pmfVkRhIl28yXWyzotN9F7kZIDfF3L7-giek"
+  },
+  
 };
 
 /*
